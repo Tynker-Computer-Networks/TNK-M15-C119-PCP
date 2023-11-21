d1c23e745463a9cf0a892a0bd27306d8e2333435
Vishal Gaddam <41083456+vishalgaddam873@users.noreply.github.com>
2023-11-21 12:01:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,67 +15,75 @@ class SecretSanta(Tk):
         self.frame = Frame()
         self.frame.grid(column=0, row=0, padx=10, pady=10)
 
-        Label(self.frame, text="Participant Name:").grid(column=0, row=0, sticky="w")
+        Label(self.frame, text="Participant Name:").grid(
+            column=0, row=0, sticky="w")
         self.participant_name_entry = Entry(self.frame)
-        self.participant_name_entry.grid(column=1, row=0, padx=10, pady=5, columnspan=2)
+        self.participant_name_entry.grid(
+            column=1, row=0, padx=10, pady=5, columnspan=2)
 
-        Label(self.frame, text="Participant Email:").grid(column=0, row=1, sticky="w")
+        Label(self.frame, text="Participant Email:").grid(
+            column=0, row=1, sticky="w")
         self.participant_email_entry = Entry(self.frame)
-        self.participant_email_entry.grid(column=1, row=1, padx=10, pady=5, columnspan=2)
+        self.participant_email_entry.grid(
+            column=1, row=1, padx=10, pady=5, columnspan=2)
 
-        # Create a Label with the name Participant Comma Seprated Wishlist.
-        Label(self.frame, text="Participant Comma Seprated Wishlist:").grid(column=0, row=2, sticky="w")
+        # Create a Label with the name Participant Wishlist.
+        Label(self.frame, text="Participant Wishlist:").grid(
+            column=0, row=2, sticky="w")
         self.participant_whishlist_entry = Entry(self.frame)
-        self.participant_whishlist_entry.grid(column=1, row=2, padx=10, pady=5, columnspan=2)
+        self.participant_whishlist_entry.grid(
+            column=1, row=2, padx=10, pady=5, columnspan=2)
 
         # Create a button with the name Add Participant.
-        self.add_button = Button(self.frame, text="Add Participant", command=self.add_participant)
+        self.add_button = Button(
+            self.frame, text="Add Participant", command=self.add_participant)
         self.add_button.grid(column=1, row=3, padx=10, pady=5, columnspan=2)
 
-        # Create a Listbox with the name Participants List.
-        Label(self.frame, text="Participants List:").grid(column=0, row=4, sticky="w")
+        Label(self.frame, text="Participants List:").grid(
+            column=0, row=4, sticky="w")
         self.participants_listbox = Listbox(self.frame)
-        self.participants_listbox.grid(column=1, row=4, padx=10, pady=5, columnspan=2)
+        self.participants_listbox.grid(
+            column=1, row=4, padx=10, pady=5, columnspan=2)
 
-        Label(self.frame, text="Sender's Email:").grid(column=0, row=5, sticky="w")
+        Label(self.frame, text="Sender's Email:").grid(
+            column=0, row=5, sticky="w")
         self.sender_email_entry = Entry(self.frame)
-        self.sender_email_entry.grid(column=1, row=5, padx=10, pady=5, columnspan=2)
+        self.sender_email_entry.grid(
+            column=1, row=5, padx=10, pady=5, columnspan=2)
 
-        Label(self.frame, text="Sender's Password:").grid(column=0, row=6, sticky="w")
+        Label(self.frame, text="Sender's Password:").grid(
+            column=0, row=6, sticky="w")
         self.sender_password_entry = Entry(self.frame, show="*")
-        self.sender_password_entry.grid(column=1, row=6, padx=10, pady=5, columnspan=2)
+        self.sender_password_entry.grid(
+            column=1, row=6, padx=10, pady=5, columnspan=2)
 
-        # Create a Label with the name Attached Files.
-        self.attached_files_label = Label(self.frame, text="Attached Files:")
-        self.attached_files_label.grid(column=0, row=7, sticky="w", padx=10, pady=5)
-
-        # Create a button with the name Attached File.
-        self.attach_button = Button(self.frame, text="Attach File")
-        self.attach_button.grid(column=1, row=8, padx=10, pady=5, columnspan=2)
-
-        # Create a button with the name Send Assignments and on button press call handle_assignments function.
-        self.send_button = Button(self.frame, text="Send Assignments", command=self.handle_assignments)
+        # Create a button with the name Notify Santa and on button press call handle_assignments function.
+        self.send_button = Button(
+            self.frame, text="Notify Santa", command=self.handle_assignments)
         self.send_button.grid(column=1, row=11, padx=10, pady=10, columnspan=2)
 
         self.status_label = Label(self.frame, text="", fg="green")
-        self.status_label.grid(column=1, row=12, padx=10, pady=10, columnspan=2)
+        self.status_label.grid(column=1, row=12, padx=10,
+                               pady=10, columnspan=2)
+
+        # Declare the variable isEmailNotSent to check the email is sent or not to the participiants.
+        self.isEmailNotSent = False
 
         # List to keep track of participants
-        self.participants=[]
-        self.assignments=[]
+        self.participants = []
+        self.assignments = []
 
     # Function to add the participant to the list.
     def add_participant(self):
         name = self.participant_name_entry.get()
         email = self.participant_email_entry.get()
         wishlist = self.participant_whishlist_entry.get()
-        if(name and email and wishlist):
+        if (name and email and wishlist):
             self.participants.append((name, email, wishlist))
             self.update_listbox()
             self.participant_name_entry.delete(0, END)
             self.participant_email_entry.delete(0, END)
             self.participant_whishlist_entry.delete(0, END)
-    
 
     # Function to update the listbox
     def update_listbox(self):
@@ -89,35 +97,34 @@ class SecretSanta(Tk):
 
     # Function to send Secret Santa assignments
     def send_assignments(self):
-        # Check atleast 3 participents are there
+
         if len(self.participants) < 3:
-            self.status_label.config(text="At least 3 participants are required.")
+            self.status_label.config(
+                text="At least 3 participants are required.")
             return
-        
-        # Shuffle the list of participants
+
         random.shuffle(self.participants)
 
-        # Create a copy of participants for assignments
         self.assignments = self.participants.copy()
-        
-        # Shuffle the list of assignments
+
         while self.has_self_assignments():
             random.shuffle(self.assignments)
 
-        # To each participant send the assignment email.
         for i, participant in enumerate(self.participants):
-            # Below are recipient name and email
             name, email, w = participant
             assigned_to = self.assignments[i][0]
             wishlist = self.assignments[i][2]
 
             self.send_email(name, email, assigned_to, wishlist)
-        
-        # Show the message box after sending the assignment.
-        messagebox.showinfo("Assignments Sent", "Assignments sent. Check your emails!")
 
+        # Check if email is sent or not using variable isEmailNotSent.
+        if (self.isEmailNotSent == False):
+            # Show the message box after sending the assignment.
+            messagebox.showinfo("Assignments Sent",
+                                "Assignments sent. Check your emails!")
 
     # Function to check for self-assignments
+
     def has_self_assignments(self):
         for i in range(len(self.participants)):
             if self.participants[i][0] == self.assignments[i][0]:
@@ -125,7 +132,7 @@ class SecretSanta(Tk):
         return False
 
     # Function to send email assignments
-    def send_email(self,recipient_name, recipient_email, assigned_to, wishlist):
+    def send_email(self, recipient_name, recipient_email, assigned_to, wishlist):
         try:
             sender_email = self.sender_email_entry.get()
             password = self.sender_password_entry.get()
@@ -136,19 +143,21 @@ class SecretSanta(Tk):
 
             subject = "Your Secret Santa Assignment"
             msg_body = f"Hello {recipient_name},\n\nYour Secret Santa assignment is: {assigned_to}\n\nWishlist: {wishlist}"
-           
+
             message = MIMEMultipart()
             message["From"] = sender_email
             message["To"] = recipient_email
             message["Subject"] = subject
             message.attach(MIMEText(msg_body, "plain"))
 
-            smtp_server.sendmail(sender_email, recipient_email, message.as_string())
+            smtp_server.sendmail(
+                sender_email, recipient_email, message.as_string())
             smtp_server.quit()
 
         except Exception as e:
-            # Replace print with messagebox.showerror()
-            messagebox.showerror("Error",f"An error occurred:{str(e)}")
+            # Replace print with messagebox.showerror() and make the isEmailNotSent to True
+            self.isEmailNotSent = True
+            messagebox.showerror("Error", f"An error occurred:{str(e)}")
 
 
 def main():
@@ -156,6 +165,7 @@ def main():
     # Instead of send_email call mainloop() function
     # app.send_email()
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
